2cbb964c9f8f783a77c6ddeea56d98e2447ac03c
wjd14 <will@ayerdoyles.org>
2023-03-09 17:05:08 -0500

    auto and gotointake

--- src/main/java/frc/robot/commands/AutonomousCommand.java
+++ src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import frc.robot.Constants;
 import frc.robot.subsystems.*;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -8,20 +9,28 @@ import edu.wpi.first.wpilibj.Timer; // has methods restart() and get()
 
 public class AutonomousCommand extends CommandBase {
 
+    private final DriveTrain m_driveTrain;
+    private final Timer m_autoTimer;
 
-    public AutonomousCommand() {
-        // m_subsystem = subsystem;
-        // addRequirements(m_subsystem);    
+    public AutonomousCommand(DriveTrain autoDrive) {
+        // m_subsystem = subsystem
+        // addRequirements(m_subsystem);
+        m_driveTrain = autoDrive;
+        addRequirements(m_driveTrain);
+        m_autoTimer = new Timer();
+
     }
 
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        m_autoTimer.restart();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        m_driveTrain.drive(Constants.AutoConstants.autoSpeed, 0);
     }
 
     // Called once the command ends or is interrupted.
@@ -32,7 +41,12 @@ public class AutonomousCommand extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        if(m_autoTimer.get() >= Constants.AutoConstants.autoTime){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     @Override
